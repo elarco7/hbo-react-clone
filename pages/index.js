c9bf655db73ae4271d45fdd49e859b2a4b282729
elarco7 <wepro7@gmail.com>
2023-05-23 16:01:16 -0500

    changed stylying for loggin user img

--- pages/index.js
+++ pages/index.js
@@ -15,15 +15,7 @@ export default function Home() {
               src="https://randomuser.me/api/portraits/women/57.jpg"
               alt="user pic"
               className="login-user__user-img"
-              style={{
-                width: "125px",
-                height: "125px",
-                borderRadius: "50%",
-                marginBottom: "1rem",
-                border: "3px solid rgb(132,0,255)",
-                objectFit: "cover",
-                objectPosition: "center",
-              }}
+              layout="fill"
             />
             <div className="login-user__user-name">Kelly</div>
           </div>
